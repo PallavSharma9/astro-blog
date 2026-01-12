08996db46302d5b772915931d18bb90c6983ff86
PallavSharma9 <pallavsharma0202@gmail.com>
2026-01-12 10:19:47 +0530

    Adding content and 404 error

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION =
+  " Articles, Stories and tutorials from Tech People";
+
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
